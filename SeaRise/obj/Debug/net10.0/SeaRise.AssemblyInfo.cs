3c3a894e047df09cda949160db839ca7a91379c4
diogo2079721 <2079721@student.uma.pt>
2025-12-11 12:54:33 +0000

    ADD - IMAGES TO TURISMO

--- SeaRise/obj/Debug/net10.0/SeaRise.AssemblyInfo.cs
+++ SeaRise/obj/Debug/net10.0/SeaRise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeaRise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fa248a5f98c451037d3ec36077aaf2bf5c41b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f0bc5b5a83e19dc1d9eba93c1555843600b296")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeaRise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeaRise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
